8c73e57a52ae23e84adb5113a78473339669307e
Daniel Huffman <drhuffman12@gmail.com>
2020-01-23 00:26:47 -0500

    attempt to add '#to_json' to State

--- src/aasm/state.cr
+++ src/aasm/state.cr
@@ -1,4 +1,15 @@
 struct AASM::State
+  include JSON::Serializable
+  def to_json
+    {
+      opts: @opts
+      # ,
+      # enter: @enter,
+      # guard: @guard
+    }.to_json
+  end
+  # Error: no overload matches 'Proc(Nil)#to_json' with type JSON::Builder
+  
   getter enter : (-> Nil)?
   getter guard : (-> Bool)?
 
